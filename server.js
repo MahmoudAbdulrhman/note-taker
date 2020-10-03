35fb0421b179f336ed438cf11676f56844b15b3f
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-10-03 01:32:18 -0700

    add routes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(__dirname));
 
 //Require routes file
-require('./routes/routes')(app);
+require("./routes/htmlRoutes")(app);
 
 // Setup listener
 app.listen(PORT, function() {
